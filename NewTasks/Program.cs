218586c975acc275891bc1fe4c007bc6efb7eeff
Yury <110132830+Mezhdunami@users.noreply.github.com>
2023-07-21 13:07:44 +0300

    Develop2 (#2)
    * Task 13 - Commit changes and push
    
    * Task 13

--- NewTasks/Program.cs
+++ NewTasks/Program.cs
@@ -2,3 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello again!");
 Console.WriteLine("22222");
+
+Console.WriteLine("Develop2 Task - Hello, Denis!");
+Console.WriteLine("TASK 13!");
