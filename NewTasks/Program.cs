42d65043f31e6af95dfd9aa790409f4a748cdcc4
Yury <taskin.jura@gmail.com>
2023-07-23 20:03:36 +0300

    del

--- NewTasks/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello again!");
-Console.WriteLine("22222");
-
-Console.WriteLine("Develop2 Task - Hello, Denis!");
-Console.WriteLine("TASK 13!");
-
-Console.WriteLine("Task 20 VENTIUM");
